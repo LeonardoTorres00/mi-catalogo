d87933dbf785365a1f62273e9ea3f02a270b6b5e
LeonardoTorres00 <56545632+LeonardoTorres00@users.noreply.github.com>
2020-05-21 20:38:58 -0500

    E2 A4

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AUTOMOVILES } from '../data';
 import { Automovil } from '../models';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-list',
@@ -9,10 +10,36 @@ import { Automovil } from '../models';
 })
 export class ListComponent implements OnInit {
   autos: Automovil[];
-  constructor() { }
+  autoSeleccionado: Automovil;
+
+  closeResult = '';
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.autos = AUTOMOVILES;
+  }
+
+  onSelect(auto: Automovil) {
+    this.autoSeleccionado = auto;
+  }
+
+  open(auto: Automovil, content) {
+    this.autoSeleccionado = auto;
+    this.modalService.open(content, { centered: true }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
