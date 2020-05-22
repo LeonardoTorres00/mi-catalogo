a9ffbaf292fe68a746eeaed640a3d3e40e4c7c8d
LeonardoTorres00 <56545632+LeonardoTorres00@users.noreply.github.com>
2020-05-22 15:26:49 -0500

    E3 A7

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AUTOMOVILES } from '../data';
+import { Automovil } from '../models';
 
 @Component({
   selector: 'app-table',
@@ -10,6 +12,16 @@ export class TableComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  page = 1;
+  pageSize = 4;
+  collectionSize = AUTOMOVILES.length;
+
+  get autos(): Automovil[] {
+    return AUTOMOVILES
+      .map((autos, i) => ({id: i + 1, ...autos}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
   }
 
 }
